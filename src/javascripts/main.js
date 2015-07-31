4da986c163f36cc7be8ae0bcac093601f27ba4a8
kiwasa <iwasa@team-lab.com>
2015-07-31 18:18:52 +0900

    mainjsの書き方メモを追記

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,19 +1,25 @@
 var $ = require('jquery');
+window.$ - $;
+window.jQuery = $;
+/*
+var hoge = require('./browserify_libs/hoge.js');
+jQueryプラグインを追加する書き方
+*/
 
 $(function(){
-    $("a[href^=#]").on('click', function(e){
-        e.preventDefault();
+  $("a[href^=#]").on('click', function(e){
+    e.preventDefault();
 
-        var href = $(this).attr("href");
-        var speed = 500;
-        var easing = 'swing';
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var animateParam = {
-            scrollTop: target.offset().top
-        }
+    var href = $(this).attr("href");
+    var speed = 500;
+    var easing = 'swing';
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var animateParam = {
+        scrollTop: target.offset().top
+    }
 
-        $("html, body").animate(animateParam, speed, easing);
-    });
+    $("html, body").animate(animateParam, speed, easing);
+  });
 });
 
 
