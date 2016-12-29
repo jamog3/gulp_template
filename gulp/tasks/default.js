06d070f3202f1aabfb8df2a62dea15014b66f048
kiwasa <iwasa@team-lab.com>
2016-12-29 14:44:38 +0900

    spriteタスクもgulp defaultから除外

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,15 +2,12 @@ var gulp = require('gulp');
 
 // default
 gulp.task('default', [
-  // 'server',
   'copy',
   'html',
   'css',
-  'sprite',
   'imgMin',
   'imgMinPng',
   'js',
   'jsMin',
-  // 'watch'
 ]);
 
